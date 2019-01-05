c95ebc632cf7d9d8c84b2243e928340135838360
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-01-05 10:32:16 +0700

    feat(config): convert eslint config json into js format

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import { render } from 'react-dom';
 const Button = lazy(() => import('./component/button'));
 
 render(
-  <div>
-    <Suspense fallback={<div>Loading...</div>}>
-      <h1>Datas</h1>
-      <Button />
-    </Suspense>
-  </div>,
-  document.getElementById('app')
+    <div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <h1>Datas</h1>
+            <Button />
+        </Suspense>
+    </div>,
+    document.getElementById('app')
 );
